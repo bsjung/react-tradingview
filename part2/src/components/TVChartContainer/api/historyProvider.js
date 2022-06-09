1521b157d40687d6e9b6fd66432c6cd87a808657
Benjamin Jung <bsjung@gmail.com>
2022-06-09 19:49:14 +0900

    Update part2 : refacto historyProvider.js with async/await.

--- part2/src/components/TVChartContainer/api/historyProvider.js
+++ part2/src/components/TVChartContainer/api/historyProvider.js
@@ -6,7 +6,7 @@ const history = {}
 export default {
 	history: history,
 
-    getBars: function(symbolInfo, resolution, from, to, first, limit) {
+    getBars: async function(symbolInfo, resolution, from, to, first, limit) {
 		var split_symbol = symbolInfo.name.split(/[:/]/)
 			const url = resolution === 'D' ? '/data/histoday' : resolution >= 60 ? '/data/histohour' : '/data/histominute'
 			const qs = {
@@ -19,36 +19,34 @@ export default {
 				}
 			// console.log({qs})
 
-        return rp({
-                url: `${api_root}${url}`,
-                qs,
-            })
-            .then(data => {
-                console.log({data})
-				if (data.Response && data.Response === 'Error') {
-					console.log('CryptoCompare API error:',data.Message)
-					return []
-				}
-				if (data.Data.length) {
-					console.log(`Actually returned: ${new Date(data.TimeFrom * 1000).toISOString()} - ${new Date(data.TimeTo * 1000).toISOString()}`)
-					var bars = data.Data.map(el => {
-						return {
-							time: el.time * 1000, //TradingView requires bar time in ms
-							low: el.low,
-							high: el.high,
-							open: el.open,
-							close: el.close,
-							volume: el.volumefrom 
-						}
-					})
-						if (first) {
-							var lastBar = bars[bars.length - 1]
-							history[symbolInfo.name] = {lastBar: lastBar}
-						}
-					return bars
-				} else {
-					return []
+        const data = await rp({
+			url: `${api_root}${url}`,
+			qs,
+		})
+		console.log({ data })
+		if (data.Response && data.Response === 'Error') {
+			console.log('CryptoCompare API error:', data.Message)
+			return []
+		}
+		if (data.Data.length) {
+			console.log(`Actually returned: ${new Date(data.TimeFrom * 1000).toISOString()} - ${new Date(data.TimeTo * 1000).toISOString()}`)
+			var bars = data.Data.map(el => {
+				return {
+					time: el.time * 1000,
+					low: el.low,
+					high: el.high,
+					open: el.open,
+					close: el.close,
+					volume: el.volumefrom
 				}
 			})
+			if (first) {
+				var lastBar = bars[bars.length - 1]
+				history[symbolInfo.name] = { lastBar: lastBar }
+			}
+			return bars
+		} else {
+			return []
+		}
 }
 }
