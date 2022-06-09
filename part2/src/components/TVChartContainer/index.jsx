2fc684be141c77a91713e40ae77ebda3b6456f14
Benjamin Jung <bsjung@gmail.com>
2022-06-09 23:07:49 +0900

    Update public/index.html : charting library path

--- part2/src/components/TVChartContainer/index.jsx
+++ part2/src/components/TVChartContainer/index.jsx
@@ -15,7 +15,7 @@ export class TVChartContainer extends React.PureComponent {
 		symbol: 'Coinbase:BTC/USD',
 		interval: '15',
 		containerId: 'tv_chart_container',
-		libraryPath: '/charting_library/',
+		libraryPath: '/static/charting_library/',
 		chartsStorageUrl: 'https://saveload.tradingview.com',
 		chartsStorageApiVersion: '1.1',
 		clientId: 'tradingview.com',
